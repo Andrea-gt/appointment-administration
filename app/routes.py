32e2d9c5288619e523f7c39e1fcb6c1dfcb1a5bd
Andrea-gt <a.ximeram@gmail.com>
2021-09-29 17:48:43 -0600

    Create HTML Code and Add Requierements

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from usuario import usuario
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+
+    psicologo = usuario("Andrea")
+    user = {'username': psicologo.nombreUsuario}
+    return render_template('index.html', title='Inicio', user=user)
---
+++ usuario.py
@@ -0,0 +1,6 @@
+#Clase Usuario; Clase que determina los datos del psicologo.
+class usuario:
+    nombreUsuario = ""
+
+    def __init__(self, nombreUsuario):
+        self.nombreUsuario = nombreUsuario
