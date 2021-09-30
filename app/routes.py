15ab6b50447683c9fd6a0ccb904068a885823d00
Andrea-gt <a.ximeram@gmail.com>
2021-09-30 15:54:40 -0600

    Start constructing DB

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,19 @@
 from usuario import usuario
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 @app.route('/index')
 def index():
 
     psicologo = usuario("Andrea")
     user = {'username': psicologo.nombreUsuario}
-    return render_template('index.html', title='Inicio', user=user)
+
+    if request.method == "POST":
+        name_content = request.form["content"]
+
+    else:
+        return render_template('index.html', title='Inicio', user=user)
+
+    
