9eb14b32e2d79999b92cbf037f075291dedb2f6d
FabianJuarez182 <jua21440@uvg.edu.gt>
2021-10-08 17:12:11 -0600

    base de datos creada y modicacion de error minimo

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, request, redirect, url_for
+from flask import render_template, flash, request, redirect
 from app.forms import LoginForm, EnteringStudentData, EnteringDatesData, EnteringHistoryData
 
 @app.route('/')
@@ -14,7 +14,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        return redirect('/index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/')
